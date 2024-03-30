824ab91abc9c948fca05ea2f8f93fa582aea26ec
y2chan <wcn07062@gmail.com>
2024-03-30 15:30:32 +0900

    가상환경 설정 + RAG 테스트 완료

---
+++ ragtest.py
@@ -0,0 +1,63 @@
+from langchain import hub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+# 단계 1: 문서 로드(Load Documents)
+# 문서를 로드하고, 청크로 나누고, 인덱싱합니다.
+from langchain_community.document_loaders import TextLoader
+
+loader = TextLoader("data/SYU_GPT data.txt")
+docs = loader.load()
+
+# 단계 2: 문서 분할(Split Documents)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+
+splits = text_splitter.split_documents(docs)
+
+# 단계 3: 임베딩 & 벡터스토어 생성(Create Vectorstore)
+# 벡터스토어를 생성합니다.
+vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+# 단계 4: 검색(Search)
+# 뉴스에 포함되어 있는 정보를 검색하고 생성합니다.
+retriever = vectorstore.as_retriever()
+
+# 단계 5: 프롬프트 생성(Create Prompt)
+# 프롬프트를 생성합니다.
+prompt = hub.pull("rlm/rag-prompt")
+
+# 단계 6: 언어모델 생성(Create LLM)
+# 모델(LLM) 을 생성합니다.
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+
+def format_docs(docs):
+    # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+# 단계 7: 체인 생성(Create Chain)
+rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+)
+
+# 단계 8: 체인 실행(Run Chain)
+# 문서에 대한 질의를 입력하고, 답변을 출력합니다.
+question = "교내장학금에는 어떤게 있어?"
+response = rag_chain.invoke(question)
+
+# 결과 출력
+print(f"문서의 수: {len(docs)}")
+
+# 10번째 페이지의 내용 출력
+print(f"\n[페이지내용]\n{docs[0].page_content[:500]}")
+print(f"\n[metadata]\n{docs[0].metadata}\n")
+print("===" * 20)
+print(f"[HUMAN]\n{question}\n")
+print(f"[AI]\n{response}")
